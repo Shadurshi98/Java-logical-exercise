e874f4bbcfb77c4bd17f82900a54b5e668fc5b20
Shadurshi98 <shadurshig@gmail.com>
2023-01-10 17:16:26 +0530

    change

--- src/Exercise1_1/Exercise1_1.java
+++ src/Exercise1_1/Exercise1_1.java
@@ -3,18 +3,15 @@ package Exercise1_1;
 public class Exercise1_1 {
 
 	public static void main(String[] args) {
-int[] myNum = {1,2,3,4,5,6,7,8,9,10};
-int sum =0;
+		float[] myNum = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		float sum = 0;
 
-for (int i=0;i<myNum.length;i++) {
-	
-	sum= sum + myNum[i];
-	}
-	double average = sum / myNum.length;
-System.out.print("The average is :" + average);
+		for (int i = 0; i < myNum.length; i++) {
 
-
-
+			sum = sum + myNum[i];
+		}
+		double average = sum / myNum.length;
+		System.out.print("The average is :" + average);
 
 	}
 
